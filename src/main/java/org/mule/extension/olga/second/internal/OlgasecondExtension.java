3c5087fa46840e6eebe7900f975b81809b0ba445
olgakons <okonstantinov@salesforce.com>
2023-10-27 12:57:40 +0200

    Adds requires entitlement - licence generation

--- src/main/java/org/mule/extension/olga/second/internal/OlgasecondExtension.java
+++ src/main/java/org/mule/extension/olga/second/internal/OlgasecondExtension.java
@@ -3,6 +3,7 @@ package org.mule.extension.olga.second.internal;
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Configurations;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.runtime.extension.api.annotation.license.RequiresEntitlement;
 
 
 /**
@@ -12,6 +13,7 @@ import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 @Xml(prefix = "olga--second")
 @Extension(name = "Olga--second")
 @Configurations(OlgasecondConfiguration.class)
+@RequiresEntitlement(name="Olga--second")
 public class OlgasecondExtension {
 
 }
